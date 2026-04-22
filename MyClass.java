5bbf8ffd4aab6229f101d0ec5d7653b0fbacce2c
saicharan <saicharan.epparapally@gmail.com>
2026-04-22 09:53:39 +0530

    Edited

--- MyClass.java
+++ MyClass.java
@@ -1,13 +1,12 @@
 
 
 
-puclic class Myclass{
-}public class MyClass {
-    public static void main(String[] args) {
+
+public class MyClass {
+
+    public static void main(String[] args)
+    {
         System.out.println("Hello, Sai Charan!");
-        System.out.println("Hellooooooo");
-        System.out.println("deeufuefeefefefefef");
-         if(1>=1
-{
-  // edited in Dev1}
+	System.out.println("Sai, is that you!!");         
+    }
 }
